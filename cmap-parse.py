1a0e34044135d5229a4660a12db0f05ebf9a18cb
Josh Pelkey <joshpelkey@gmail.com>
2015-09-28 07:51:12 -0400

    new crosslinks style
    find common nodes and eliminate common edges between hierarchies. still
    an issue with cycles

--- cmap-parse.py
+++ cmap-parse.py
@@ -88,7 +88,6 @@ for cmap_file in files:
 	cross_links_list = []
 	totalcrosslinks = 0
 	for x in (list (itertools.combinations (subgraph_list, r=2))):
-
 		# update 9/23/2015 -- i need to do this to a copy of the graph, ow i'm altering the graph for some comparisons
 		xcopy = []
 		xcopy.append (G.subgraph(x[0]).copy())
@@ -98,64 +97,36 @@ for cmap_file in files:
                 
 		# list comprehension python leetness ftw
 		common_edges = (list (n for n in xcopy[0].edges () if n in xcopy[1].edges ()))
+		common_nodes = (list (n for n in xcopy[0].nodes () if n in xcopy[1].nodes ()))
 
-		# Update Sept 26, 2015
-		# I think I need to find common nodes and then count all edges that are not the same
-		
-		# if not empty set, print some stuff
-		#if (common_edges):
-			#print (common_edges)
-			
-		# remove the common edges
-		xcopy[0].remove_edges_from (common_edges)
-		xcopy[1].remove_edges_from (common_edges)
-		
-		#print ('Reprint subgraph')
-		#print (xcopy[0].adj)
-		#print (xcopy[1].adj)
-		
-		# should remove nodes that are not connected to anything
-		xcopy[0].remove_nodes_from (n for n in xcopy[0].nodes () if xcopy[0].degree(n) == 0)
-		xcopy[1].remove_nodes_from (n for n in xcopy[1].nodes () if xcopy[1].degree(n) == 0)
-		
-		#print ('ReReprint subgraph')
-		#print (xcopt[0].adj)
-		#print (xcopy[1].adj)
-		
-		# now count the in degree for common nodes (this is crosslinks?)
-		# seems like current method will count links from the same hierarchy, but this might be best, 
-		# since we can't really say which heirarchy is given to the concept
-		for cl in (xcopy[0].nodes ()):
-			cross_links = {}
-			if cl in xcopy[1].nodes ():
-				cross_links[cl] = xcopy[0].in_degree (cl) + xcopy[1].in_degree (cl)
-				totalcrosslinks = totalcrosslinks + (xcopy[0].in_degree (cl) + xcopy[1].in_degree (cl))
-				cross_links_list.append (cross_links)
-				#print ('Adding crosslinks at ' + cl + ': in_degree(0): ' + str(xcopy[0].in_degree (cl)) + " in_degree(1): " + str(xcopy[1].in_degree (cl)))
-				#print (cross_links_list)
-
-		# I think there is an edge case here where crosslinks come in to the top level heirarchy
-		# We probably have to count the in_degree - 1 for all heirarchies that don't come from top (sustain.)
-
-
-	# total up all the cross links
-	# update 9/23 -- can I divide by the total number of heirarchies to match our 'manual' counts?
-	#print (cross_links_list)
-	#print (totalcrosslinks)
-	total_crosslinks_list = [sum (n.values ()) for n in cross_links_list]
-
-	if not total_crosslinks_list:
-		total_crosslinks_list.append (0)
-
-	total_crosslinks = sum (total_crosslinks_list)
+		# get all of the edges for the common node
+		for x in common_nodes:
+			inEdges0 = list (xcopy[0].in_edges(x))
+			outEdges0 = list (xcopy[0].out_edges(x))
+			inEdges1 = list (xcopy[1].in_edges(x))
+			outEdges1 = list (xcopy[1].out_edges(x))
+			node_common_edges = []
+			if inEdges0:
+				node_common_edges.append (inEdges0)
+			if inEdges1:
+				node_common_edges.append (inEdges1)
+			if outEdges0:
+				node_common_edges.append (outEdges0)
+			if outEdges1:
+				node_common_edges.append (outEdges1)
+				
+			crosslink_edges = (list (n for n in node_common_edges if n[0] not in common_edges))
+			totalcrosslinks += len (crosslink_edges)
+				
+	
 
 	# print out the stuffs
 	print ('>> Num Concepts: ' + str (num_concepts))
 	print ('>> Heirarchy: ' + str (heirarchy))
 	print ('>> HH: ' + str (highest_heir))
-	print ('>> CL: ' + str (total_crosslinks))
+	print ('>> CL: ' + str (totalcrosslinks))
 	# show me cycles
-	#print ('>> Cycles: ' + str (nx.simple_cycles (G)))
+	print ('>> Cycles: ' + str (nx.simple_cycles (G)))
 
 	# make it pretty
 	print ('\n')
