aeef57886ef52b2bb92c67428443a2b37344429c
Josh Pelkey <joshpelkey@gmail.com>
2015-10-06 21:30:11 -0400

    some early gui updates

--- gui.py
+++ gui.py
@@ -1,10 +1,11 @@
+import os
 import wx
 
 class Example(wx.Frame):
 
     def __init__(self, parent, title):    
         super(Example, self).__init__(parent, title=title, 
-            size=(450, 350))
+            size=(750, 365))
 
         self.InitUI()
         self.Centre()
@@ -18,7 +19,7 @@ class Example(wx.Frame):
 
         text1 = wx.StaticText(panel, label="cmap-parse: parsing cmaps and stuff")
         sizer.Add(text1, pos=(0, 0), flag=wx.TOP|wx.LEFT|wx.BOTTOM, 
-            border=15)
+            border=10)
 
         #icon = wx.StaticBitmap(panel, bitmap=wx.Bitmap('info.png'))
         #sizer.Add(icon, pos=(0, 4), flag=wx.TOP|wx.RIGHT|wx.ALIGN_RIGHT, 
@@ -28,13 +29,17 @@ class Example(wx.Frame):
         sizer.Add(line, pos=(1, 0), span=(1, 5), 
             flag=wx.EXPAND|wx.BOTTOM, border=10)
 
-        text2 = wx.StaticText(panel, label="Name")
+        # Name the root node
+        text2 = wx.StaticText(panel, label="Root Concept")
         sizer.Add(text2, pos=(2, 0), flag=wx.LEFT, border=10)
 
         tc1 = wx.TextCtrl(panel)
         sizer.Add(tc1, pos=(2, 1), span=(1, 3), flag=wx.TOP|wx.EXPAND)
+        tc1.ChangeValue("Sustainability")
 
-        text3 = wx.StaticText(panel, label="Package")
+
+        # Import the results choice
+        text3 = wx.StaticText(panel, label="Import concept maps...")
         sizer.Add(text3, pos=(3, 0), flag=wx.LEFT|wx.TOP, border=10)
 
         tc2 = wx.TextCtrl(panel)
@@ -43,42 +48,116 @@ class Example(wx.Frame):
 
         button1 = wx.Button(panel, label="Browse...")
         sizer.Add(button1, pos=(3, 4), flag=wx.TOP|wx.RIGHT, border=5)
+        self.Bind(wx.EVT_BUTTON, self.OnOpen, button1)
 
-        text4 = wx.StaticText(panel, label="Extends")
+
+        # Export the results choice
+        text4 = wx.StaticText(panel, label="Export results to...")
         sizer.Add(text4, pos=(4, 0), flag=wx.TOP|wx.LEFT, border=10)
 
-        combo = wx.ComboBox(panel)
-        sizer.Add(combo, pos=(4, 1), span=(1, 3), 
+        tc3 = wx.TextCtrl(panel)
+        sizer.Add(tc3, pos=(4, 1), span=(1, 3), 
             flag=wx.TOP|wx.EXPAND, border=5)
 
         button2 = wx.Button(panel, label="Browse...")
         sizer.Add(button2, pos=(4, 4), flag=wx.TOP|wx.RIGHT, border=5)
 
+        # Optional Attributes which do nothing
         sb = wx.StaticBox(panel, label="Optional Attributes")
 
         boxsizer = wx.StaticBoxSizer(sb, wx.VERTICAL)
-        boxsizer.Add(wx.CheckBox(panel, label="Public"), 
+        boxsizer.Add(wx.CheckBox(panel, label=" Use The Force"), 
             flag=wx.LEFT|wx.TOP, border=5)
-        boxsizer.Add(wx.CheckBox(panel, label="Generate Default Constructor"),
+        boxsizer.Add(wx.CheckBox(panel, label=" Cast Hogwarts Spell"),
             flag=wx.LEFT, border=5)
-        boxsizer.Add(wx.CheckBox(panel, label="Generate Main Method"), 
+        boxsizer.Add(wx.CheckBox(panel, label=" Enter the Matrix"), 
             flag=wx.LEFT|wx.BOTTOM, border=5)
         sizer.Add(boxsizer, pos=(5, 0), span=(1, 5), 
             flag=wx.EXPAND|wx.TOP|wx.LEFT|wx.RIGHT , border=10)
 
+        # Halp!
         button3 = wx.Button(panel, label='Help')
         sizer.Add(button3, pos=(7, 0), flag=wx.LEFT, border=10)
+        self.Bind(wx.EVT_BUTTON, self.OnHelp, button3)
 
-        button4 = wx.Button(panel, label="Ok")
+        # Run the codes
+        button4 = wx.Button(panel, label="Run")
         sizer.Add(button4, pos=(7, 3))
+        
 
-        button5 = wx.Button(panel, label="Cancel")
+        # Get me out of here
+        button5 = wx.Button(panel, label="Exit")
         sizer.Add(button5, pos=(7, 4), span=(1, 1),  
             flag=wx.BOTTOM|wx.RIGHT, border=5)
+        self.Bind(wx.EVT_BUTTON, self.OnExit, button5)
 
         sizer.AddGrowableCol(2)
         
         panel.SetSizer(sizer)
+
+
+    # Open up a directory
+    def OnOpen(self, evt):
+        print("CWD: %s\n" % os.getcwd())
+
+        # Create the dialog. In this case the current directory is forced as the starting
+        # directory for the dialog, and no default file name is forced. This can easilly
+        # be changed in your program. This is an 'open' dialog, and allows multitple
+        # file selections as well.
+        #
+        # Finally, if the directory is changed in the process of getting files, this
+        # dialog is set up to change the current working directory to the path chosen.
+        dlg = wx.FileDialog(
+            self, message="Choose a file",
+            defaultDir=os.getcwd(), 
+            defaultFile="",
+            wildcard="Text files (*.txt)|*.txt",
+            style=wx.OPEN | wx.MULTIPLE | wx.CHANGE_DIR
+            )
+
+        # Show the dialog and retrieve the user response. If it is the OK response, 
+        # process the data.
+        if dlg.ShowModal() == wx.ID_OK:
+            # This returns a Python list of files that were selected.
+            paths = dlg.GetPaths()
+
+            print('You selected %d files:' % len(paths))
+
+            for path in paths:
+                print('           %s\n' % path)
+
+        # Compare this with the debug above; did we change working dirs?
+        print("CWD: %s\n" % os.getcwd())
+
+        # Destroy the dialog. Don't do this until you are done with it!
+        # BAD things can happen otherwise!
+        dlg.Destroy()
+
+
+
+    # Help text
+    def OnHelp(self,e):
+        # A message dialog box with an OK button. wx.OK is a standard ID in wxWidgets.
+        dlg = wx.MessageDialog( self, "cmap-parse.py\n\
+An attempt to parse concept maps, exported from cmap tools...take one\n\n\
+Copyright 2015 Josh Pelkey\n\n\
+Licensed under the Apache License, Version 2.0 (the \"License\"); you may not\n\
+use this file except in compliance with the License. You may obtain a copy of\n\
+the License at\n\n\
+http://www.apache.org/licenses/LICENSE-2.0\n\n\
+Unless required by applicable law or agreed to in writing, software distributed\n\
+under the License is distributed on an \"AS IS\" BASIS, WITHOUT WARRANTIES OR\n\
+CONDITIONS OF ANY KIND, either express or implied. See the License for the specific\n\
+language governing permissions and limitations under the License."
+                                ,
+                              "cmap-parse help", wx.OK)
+        dlg.ShowModal() # Show it
+        dlg.Destroy() # finally destroy it when finished.
+
+    # Quit the program
+    def OnExit(self,e):
+	    self.Close(True)
+	    
 
 
 if __name__ == '__main__':
