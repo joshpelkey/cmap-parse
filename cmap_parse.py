bec7265c87e29780ec07c995e1bcdeeb2219e54b
Josh Pelkey <joshpelkey@gmail.com>
2016-03-31 22:17:30 -0400

    and now we can convert

--- cmap_parse.py
+++ cmap_parse.py
@@ -18,10 +18,56 @@
 ##
 
 import glob
+import re
+import os
 import itertools
 import networkx as nx
 
 
+def CxlConversion (file):
+        # get the concepts, linking phrases, and connections
+        concepts = {}
+        linking_phrases = {}
+        connections = []
+        concepts_linked = []
+        for line in f:
+                if "concept id=" in line:
+                    concept = re.findall (r'"([^"]*)"', line)
+                    concepts[concept[0]] = concept[1]
+
+                # get the linking phrases
+                if "linking-phrase id=" in line:
+                    linking_phrase = re.findall (r'"([^"]*)"', line)
+                    linking_phrases[linking_phrase[0]] = linking_phrase[1]
+
+                # get the connections
+                if "connection id=" in line:
+                    connections.append (re.findall (r'"([^"]*)"', line))
+
+        # cycle through the linking phrase list, find all lines matching from-id and to-id
+        # edges are combinations of from-id and to-id
+        for key in linking_phrases:
+                from_links = []
+                to_links = []
+                for connection in connections:
+                    # if linking phrase is in the from-id (linking phrase at beginning)
+                    if key == connection[1]:
+                        from_links.append ([linking_phrases[key],concepts[connection[2]]])
+                    # if linking phrase is in the to-id (linking phrase at the end)
+                    if key == connection[2]:
+                        to_links.append ([concepts[connection[1]], linking_phrases[key]])
+                #print to_links
+                #print from_links
+                #print "--"
+
+                # now combine the lists, to_links to from_links
+                for to_link in to_links:
+                    for  from_link in from_links:
+                        concepts_linked.append ([to_link[0], to_link[1], from_link[1]])
+                        
+        return concepts_linked
+
+        
 def CmapParse (cmap_files, result, filenames, root_concept):
                 
         # open the result file to write output
@@ -34,30 +80,35 @@ def CmapParse (cmap_files, result, filenames, root_concept):
                 # create an empty Multi-directed graph
                 G = nx.MultiDiGraph ()
 
-                # check if this is a txt (propositions as text) or cxl file
-                # if cxl file, we need to first convert it
-
                 # open a cmap text file and begin writing results
+                global f
                 f = open (cmap_file)
                 rfile.write(filenames[index] + '\t')
 
-                # split the lines in to a list
-                lines = ((f.read ()).splitlines ())
-
-                # iterate over the list and split each line
-                # in to individual lists, delimited by tab
+                # if file extension cxl, do this fun conversion
                 textFormatCorrect = True
-                for line in lines:
-                        edge = line.split ('\t')
+                if os.path.splitext(filenames[index])[1][1:] == "cxl":
+                        concepts_linked = CxlConversion(f)
+                        for edge in concepts_linked:
+                                G.add_edge (edge[0].lower(), edge[2].lower(), link=edge[1].lower())
+                else:
+                        # split the lines in to a list
+                        lines = ((f.read ()).splitlines ())
 
-                        # break if not 3 items per line
-                        textFormatCorrect = True
-                        if len(edge) != 3:
-                                rfile.write('>> Text file not formatted correctly.\n\n')
-                                textFormatCorrect = False
-                                break
+                        # iterate over the list and split each line
+                        # in to individual lists, delimited by tab
+                        for line in lines:
+                                edge = line.split ('\t')
 
-                        G.add_edge (edge[0].lower(), edge[2].lower(), link=edge[1].lower())
+                                # break if not 3 items per line
+                                textFormatCorrect = True
+                                if len(edge) != 3:
+                                        rfile.write('>> Text file not formatted correctly.\n\n')
+                                        textFormatCorrect = False
+                                        break
+
+                                G.add_edge (edge[0].lower(), edge[2].lower(), link=edge[1].lower())
+
 
                 # if the file had a line without 3 items, break completely
                 if not textFormatCorrect:
