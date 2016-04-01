a5d4b5e7e87db66cbdbe2e77036fbac509cc03c8
Josh Pelkey <joshpelkey@gmail.com>
2016-03-31 21:40:10 -0400

    conversion working

--- cmap_cxl_converter.py
+++ cmap_cxl_converter.py
@@ -23,7 +23,7 @@ import os
 import re
 
 # choose directory where cmap txt files are located
-cmap_directory = './cmaps'
+cmap_directory = './cmaps_test'
 
 # get the files
 files = glob.glob (cmap_directory + '/*.cxl')
@@ -54,8 +54,30 @@ for cxl_file in files:
         if "connection id=" in line:
             connections.append (re.findall (r'"([^"]*)"', line))
 
-    # cycle through the connections list, find all the
+    # cycle through the linking phrase list, find all lines matching from-id and to-id
+    # edges are combinations of from-id and to-id
+    concepts_linked = []
+    for key in linking_phrases:
+        from_links = []
+        to_links = []
+        for connection in connections:
+            # if linking phrase is in the from-id (linking phrase at beginning)
+            if key == connection[1]:
+                from_links.append ([linking_phrases[key],concepts[connection[2]]])
+            # if linking phrase is in the to-id (linking phrase at the end)
+            if key == connection[2]:
+                to_links.append ([concepts[connection[1]], linking_phrases[key]])
+        print to_links
+        print from_links
+        print "--"
+
+        # now combine the lists, to_links to from_links
+        for to_link in to_links:
+            for  from_link in from_links:
+                concepts_linked.append ([to_link[0], to_link[1], from_link[1]])
+
+    for item in concepts_linked:
+        print item
 
 
-    # print concepts
     print concepts.values ()
